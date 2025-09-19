c78f442c48f691701b4b00a3c7c40f4df703394b
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2025-09-19 11:50:49 -0300

    implementando actions

--- src/Machines/bookingMachine.js
+++ src/Machines/bookingMachine.js
@@ -1,28 +1,52 @@
 import { createMachine } from "xstate";
 
-const bookingMachine = createMachine({
-  id: "buy plane tickets",
-  initial: "initial",
-  states: {
-    initial: { on: { START: "search" } },
-    search: {
-      on: {
-        CONTINUE: "passengers",
-        CANCEL: "initial",
+const bookingMachine = createMachine(
+  {
+    id: "buy plane tickets",
+    initial: "initial",
+    states: {
+      initial: {
+        on: {
+          START: {
+            target: "search",
+            actions: "logInitial",
+          },
+        },
       },
-    },
-    passengers: {
-      on: {
-        DONE: "tickets",
-        CANCEL: "initial",
+      search: {
+        entry: "logSearchEntry",
+        exit: "logSearchExit",
+        on: {
+          CONTINUE: "passengers",
+          CANCEL: "initial",
+        },
       },
-    },
-    tickets: {
-      on: {
-        FINISH: "initial",
+      passengers: {
+        on: {
+          DONE: "tickets",
+          CANCEL: "initial",
+        },
+      },
+      tickets: {
+        on: {
+          FINISH: "initial",
+        },
       },
     },
   },
-});
+  {
+    actions: {
+      logInitial: () => {
+        console.log("Imprimir Inicio");
+      },
+      logSearchEntry: () => {
+        console.log("Imprimir entrada a Search");
+      },
+      logSearchExit: () => {
+        console.log("Imprimir salida de Search");
+      },
+    },
+  }
+);
 
 export { bookingMachine };
